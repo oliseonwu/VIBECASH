2a6db7a0aaf2a791618f0cce3328e36ae0cb1da8
Olisemedua Onwuatogwu <oonwuatogwu@csumb.edu>
2023-06-09 00:54:45 -0700

    Security Refactoring
    - I refactored the code in the Email page an verify page to make a code expire if it is up to 2 mins and the user hasn't entered any code.
    
    - I made sure to use an API to get the current time instead of using the system time so hackers can't cheat the system

---
+++ Vcash/assets/utilities/CurrentTime.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import {CURRENT_TIME_API_LINK } from "@env"
+
+// fetches the current time
+export const fetchCurrentTime = async () => {
+    let currentTime = null;
+
+    await axios.get(CURRENT_TIME_API_LINK)
+    .then((res)=>{
+        currentTime = res.data.datetime;
+    })
+    .catch((error)=>{
+        console.error('Error fetching current time:', error);
+    })
+    
+    return currentTime;
+    };
